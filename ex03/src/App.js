f91b77f53011e7014d18a3008c383102ecca5021
이정우 <ljw8124@gmail.com>
2022-11-28 15:08:08 +0900

    initial commit

---
+++ ex03/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import firebase from './firebase';
+
+console.log(firebase);
+
+function App() {
+  return (
+    <div >
+    </div>
+  );
+}
+
+export default App;
---
+++ ex03/src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvVEI68vKwEketII76ljqHFpE0cTzSOOI",
+  authDomain: "testproject-c6864.firebaseapp.com",
+  projectId: "testproject-c6864",
+  storageBucket: "testproject-c6864.appspot.com",
+  messagingSenderId: "163649870913",
+  appId: "1:163649870913:web:ae293cc299cef91192fac0"
+};
+
+export default initializeApp(firebaseConfig);
