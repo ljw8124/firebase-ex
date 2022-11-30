436775b5a4221ac9b2b4dfa07f409ec2b4f82927
Joungwoo Lee <ljw8124@gmail.com>
2022-11-30 10:46:53 +0900

    logout

--- ex03/src/routes/Auth.js
+++ ex03/src/routes/Auth.js
@@ -1,12 +1,19 @@
 import React, {useState} from 'react';
-import {authService,} from 'fbInstance';
-import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth";
+import {authService} from 'fbInstance';
+import {
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
   const [error, setError] = useState("");
+
   const onChange = (event) => {
     const {target: {name, value}} = event;
     if(name === "email") {
@@ -38,6 +45,18 @@ const Auth = () => {
     }
   };
   const toggleAccount = () => setNewAccount(prev => !prev);
+  const onSocialClick = async (event) => {
+    const {target: {name},
+    } = event;
+    let provider;
+    if(name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if(name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    const data = await signInWithPopup(authService, provider);
+    console.log(data);
+  }
 
   return (
       <div>
@@ -63,8 +82,8 @@ const Auth = () => {
           <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
         </form>
         <div>
-          <button>Continue with Google</button>
-          <button>Continue with Github</button>
+          <button name="google" onClick={onSocialClick}>Continue with Google</button>
+          <button name="github" onClick={onSocialClick}>Continue with Github</button>
         </div>
       </div>
   );
--- ex03/src/routes/Profile.js
+++ ex03/src/routes/Profile.js
@@ -1,3 +1,22 @@
 import React from "react";
+import { authService } from '../fbInstance';
+import { useNavigate } from 'react-router-dom';
 
-export default () => <span>Profile</span>;
+const Profile = () => {
+  const navigate = useNavigate();
+  const onLogoutClick = () => {
+    authService.signOut()
+        .then(() => navigate("/"))
+        .catch(error => console.error("ERROR:", error));
+
+  }
+
+  return (
+      <>
+        <button onClick={onLogoutClick}>Logout</button>
+      </>
+  );
+
+};
+
+export default Profile;
